4d65dd6e5f8c7790cf0e305520b06feb9b4650ca
sripriyaacharya@gmail.com <sripriyaacharya@gmail.com>
2023-02-05 13:08:32 +0000

    commit

--- models/raw_data.sql
+++ models/raw_data.sql
@@ -1,18 +1,5 @@
 
 
 
-with nba_players as (
-
-    select
-        id as customer_id,
-        first_name,
-        lastname,
-        department
-
-    from fivetran_database01.google_sheets.nba_players
-
-),
-
-
 
 
