899622c3f185c262e495fa48ee7adb4c2705b979
sripriyaacharya@gmail.com <sripriyaacharya@gmail.com>
2023-02-09 08:54:46 +0000

    commit

--- models/customers.sql
+++ models/customers.sql
@@ -4,30 +4,22 @@
   )
 }}
 
-
-
-
 with customers as (
-    
 
     select
-        id as customer_id,
-        first_name,
-        last_name
+        id as customer_id
+        ...
 
-    from raw.jaffle_shop.customers
+)
+with customers as (
+
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from  raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
