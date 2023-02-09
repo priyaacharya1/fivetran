305306a33290bc1e5a716d3e69802420895f3d15
sripriyaacharya@gmail.com <sripriyaacharya@gmail.com>
2023-02-09 09:11:09 +0000

    commit

--- models/fivetran/customer.sql
+++ models/fivetran/customer.sql
@@ -1,28 +1,26 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
 
 with customers as (
 
     select
-        id as customer_id
-        ...
+        id as customer_id,
+        first_name,
+        last_name
 
-)
-with customers as (
-
-    select * from {{ ref('stg_customers') }}
+    from raw.jaffle_shop.customers
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from raw.jaffle_shop.orders
 
 ),
-
 customer_orders as (
 
     select
